ef6ffcef3e56b2b4097823d6f078a61e1b307fdd
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-03-04 20:52:12 -0300

    :wrench: Criando o AuthContext

---
+++ src/contexts/AuthContexts.tsx
@@ -0,0 +1,58 @@
+import { createContext, useState, useEffect } from "react";
+
+import { auth, firebase } from "../services/firebase";
+
+import { AuthContextProps, ContextProps, UserLogin } from "../@types";
+export const AuthContext = createContext({} as AuthContextProps);
+
+export function AuthContextProvider(props: ContextProps) {
+  const [user, setUser] = useState<UserLogin>();
+
+  useEffect(() => {
+    const unsubscriber = auth.onAuthStateChanged((user) => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+
+        if (!displayName || !photoURL) {
+          throw new Error("Missing information from Google Account.");
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        });
+      }
+    });
+
+    return () => {
+      unsubscriber();
+    };
+  }, []);
+
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const result = await auth.signInWithPopup(provider);
+
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user;
+
+      if (!displayName || !photoURL) {
+        throw new Error("Missing information from Google Account.");
+      }
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL,
+      });
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
