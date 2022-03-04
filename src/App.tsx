541fd5e7654a8d243a19f2522531cd758fce6610
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-03-04 20:53:50 -0300

    :wrench: Adicionando o context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContexts";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" exact component={NewRoom} />
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" exact component={NewRoom} />
+      </AuthContextProvider>
     </Router>
   );
 }
