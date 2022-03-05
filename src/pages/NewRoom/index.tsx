fa0375cd28e84e550d07c2b83b522cf352e955fa
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-03-05 13:35:50 -0300

    :wrench: Adicionando função para criação de sala

--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,14 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { FormEvent, useState } from "react";
 
 import illustrationImg from "../../assets/images/illustration.svg";
 import logoImg from "../../assets/images/logo.svg";
 
 import { Button } from "../../components/Button";
-// import { useAuth } from "../../hooks/useAuth";
+import { useAuth } from "../../hooks/useAuth";
 import { SMainContent, SPageAuth } from "../../styles/SAuth";
+import { database } from "../../services/firebase";
 
 export function NewRoom() {
-  //  const { user } = useAuth();
+  const [newRoom, setNewRoom] = useState("");
+
+  const { user } = useAuth();
+  const history = useHistory();
+
+  async function handleCreateRoom(e: FormEvent) {
+    e.preventDefault();
+
+    if (newRoom.trim() === "") {
+      return;
+    }
+
+    const roomRef = database.ref("rooms");
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+      authorEmail: user?.email,
+    });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
+  }
 
   return (
     <SPageAuth>
@@ -24,8 +47,13 @@ export function NewRoom() {
         <SMainContent>
           <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
-          <form>
-            <input type="text" placeholder="Nome da sala" />
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              onChange={(e) => setNewRoom(e.target.value)}
+              value={newRoom}
+              placeholder="Nome da sala"
+            />
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
