54438a45067d6b9032fef4add13d0c4043965ea2
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-03-07 19:13:46 -0300

    :wrench: Alterando as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch as Routes,
+  Route,
+} from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContexts";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
 
 function App() {
   return (
     <Router>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" exact component={NewRoom} />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Routes>
       </AuthContextProvider>
     </Router>
   );
